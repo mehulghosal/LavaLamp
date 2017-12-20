2076ae55759313cb6e838ac2c0558d3974eebcc2
mehulghosal <mehul.ghosal@gmail.com>
2017-12-20 12:14:36 -0500

    ugh

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import sys, pygame
-#from colors import *
+from colors import *
 from random import *
 from math import *
-white = (255,255,255)
 pygame.init()
 
 
@@ -12,7 +11,6 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((maxwidth, maxheight))
 screen.fill(white)
 pygame.display.flip()
-# bgfdnmyd
 
 class Blob(pygame.sprite.Sprite):
 	def __init__(self, width, height, col):#all measurements are in pixels
@@ -47,9 +45,13 @@ class Blob(pygame.sprite.Sprite):
 			#print("bottom")
 		#print(self.rect.x, self.rect.y)
 
-		self.rect.x += int(self.dirx * 1)
-		self.rect.y += int(self.diry * 1)
+		if self.dirx > 3 or self.dirx<-3: self.dirx = randint(1,3)
+		if self.diry > 3 or self.diry<-3: self.diry = randint(1,3)
 
+		self.rect = self.rect.move(self.dirx, self.diry)
+		#self.rect.move_ip(int(self.dirx), int(self.diry))
+		#self.rect.y += int(self.diry * 1)
+		'''
 		letterPressed = args[0]
 		if letterPressed:
 			pointlist = args[1]
@@ -62,71 +64,74 @@ class Blob(pygame.sprite.Sprite):
 				if (point[0] -10 <= cenx <= point[0]+10) and (point[1] - 10 <= ceny <= point[1]+10):
 					blobList[i].dirx = 0
 					blobList[i].diry = 0
-					#print("finished", cenx, ceny)
+					#print("finished", cenx, ceny)'''
 				
 
-		# colideList = pygame.sprite.spritecollide(self, blobs, False)
-		# for i in colideList:
-		# 	if not i is self:
-		# 		self.combine(i)
+		colideList = pygame.sprite.spritecollide(self, blobs, False)
+		for i in colideList:
+			if not i is self:
+				self.combine(i)
 
-		# if self.width*self.height >= 200*200:
-		# 	self.divide()
+		if self.width*self.height >= 200*200:
+			#print(self.width, self.height)
+			self.divide()
 
 
 	 #combines two blobs if overlap, return new blob -- figure out how to tell if they overlap
 	def combine(self, another):
 		#print(self.alive(), another.alive())
-		# area1 = self.height*self.width
-		# area2 = another.height*another.width
+		area1 = self.height*self.width
+		area2 = another.height*another.width
+		#USE PYGAME RECT.INFLATE
 
+		widt = int(sqrt((area1+area2)/(2*((self.width/another.height)+(another.width/self.height)))))
+		heig = (area1+area2)/widt
 
-		# widt = int(sqrt((area1+area2)/(2*((self.width/self.height)+(another.width/another.height)))))
-		# heig = (area1+area2)/widt
-
-		widt = self.width+another.width
-		heig = self.height+another.height
+		# widt = (self.width+another.width)/2
+		# heig = (self.height+another.height)/2
+		
 
 		colr = (int((self.col[0] + another.col[0])/2), int((self.col[1] + another.col[1])/2), int((self.col[2] + another.col[2])/2))
-		nx = int((self.rect.x + self.rect.x)/2)
-		ny = int((self.rect.y + self.rect.y)/2)
+		nx = int((self.rect.x + another.rect.x)/2)
+		ny = int((self.rect.y + another.rect.y)/2)
 		direc = (self.dirx * another.dirx, self.diry * another.diry)
 
-		'''self.kill()
-								another.kill()
-								b = Blob(widt,heig,colr)
-								b.dirx = direc[0]
-								b.rect.x = nx
-								b.diry = direc[1]
-								b.rect.y = ny
-								blobs.add(b)'''
+		b = Blob(widt, heig, colr)
+		b.dirx = direc[0]
+		b.diry = direc[1]
+		blobs.add(b)
+		blobs.remove(self, another)
+		b.rect.x = nx
+		b.rect.y = ny
 
+		'''
 		if self.alive(): 
+			print(self.width, self.height)
 			another.kill()
 			self.dirx = direc[0]
 			self.diry = direc[1]
-			self.width = widt
-			self.height = heig
+			self.rect = self.rect.inflate((self.width/another.width)+0.5, (self.height/another.height)+0.5)
 			self.col = colr
 			another = None
+			print(self.width, self.height)
 
 			#print(self.width, another.width, widt, self.width)
 						
 		else:
+			print(another.width, another.height)
 			self.kill()
 			another.dirx = direc[0]
 			another.diry = direc[1]
-			another.width = widt
-			another.height = heig
+			another.rect = another.rect.inflate((self.width/another.width)+0.5, (self.height/another.height)+0.5)
 			another.col = colr
-			print(self.width, another.width, widt, another.width)
+			#print(self.width, another.width, widt, another.width)
 			self = None
+			print(another.width, another.height)'''
 
 
 	
 	def divide(self):
 		self.kill()
-		print("divide")
 		'''c1 = Blob(self.width/2, self.height, self.col)
 								c1.rect.x = self.rect.x + self.width
 								c1.rect.y = self.rect.y
@@ -135,11 +140,12 @@ class Blob(pygame.sprite.Sprite):
 								c1.rect.x = self.rect.x - self.width
 								c1.rect.y = self.rect.y
 								c2.dirx = -1'''
-		blobs.add(Blob(self.width/2, self.height, self.col, rect = (self.rect.x+self.width, self.rect.y, self.width/2, self.height)),Blob(self.width/2, self.height/2, self.col, rect = (self.rect.x-self.width, self.rect.y, self.width/2,self.height)))
-		self = None
+		blobs.add(Blob(self.width/2, self.height, self.col))#rect = (self.rect.x+self.width, self.rect.y, self.width/2, self.height)
+		blobs.add(Blob(self.width/2, self.height, self.col))#rect = (self.rect.x-self.width, self.rect.y, self.width/2,self.height)
+		
 
 def A(blobList):
-	points = [(170,160),(230,160),(100,325),(200,325),(300,325),(50,500), (350,500)]
+	points = [(170,160),(230,160),(100,325),(200,325),(300,325),(50,500), (340,500)]
 	for i in range(len(blobList)):
 		blobList[i].dirx = copysign(1, points[i][0] - blobList[i].rect.center[0]) *randint(1,3)
 		blobList[i].diry = copysign(1, points[i][1] - blobList[i].rect.center[1]) *randint(1,3)
@@ -161,21 +167,26 @@ def C(blobList):
 
 blobs = pygame.sprite.Group()
 
-for i in range(7):
+for i in range(5):
 	#			#width 			 #height 			red					  green 		 blue
 	blob = Blob(randint(80,130), randint(125, 175), (150 + randint(0,100), randint(0,50), randint(0,50)))
+	print(blob)
 	blob.rect.center = (randint(blob.width,maxwidth-blob.width), randint(blob.height, maxheight-blob.height))
 	blobs.add(blob)
 
-while not done:
+spriteList = blobs.sprites()
+
+#while not done:
+for i in range(30):
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
-			done = True
+			#done = True
+			break
 
-	spriteList = blobs.sprites()
 	
 	letter = False
 	pointlist = [None for i in range(7)]
+	'''
 	pressed = pygame.key.get_pressed()
 	if pressed[pygame.K_a]: 
 		char = "A"
@@ -196,13 +207,15 @@ while not done:
 
 			if randint(0,1) == 1: i.diry = randint(1,3)
 			else: i.diry = -1*randint(1,3)
-
+	'''
 	screen.fill(white)
 	blobs.update(letter, pointlist, spriteList)
 	blobs.draw(screen)
+	spriteList = blobs.sprites()
+
 	for sprite in spriteList:
 		pygame.draw.ellipse(screen, sprite.col, sprite.rect, 0)
 	pygame.display.flip()
 
-	clock.tick(30)
+	clock.tick(10)
 
